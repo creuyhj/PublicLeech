d70f8d8b947674d33394d7c14dde3834023be7d3
Aid3n_Pearce <61540835+Aid-3n@users.noreply.github.com>
2020-07-09 21:07:44 +0530

    Update create_r_o_m.py

--- tobrot/helper_funcs/create_r_o_m.py
+++ tobrot/helper_funcs/create_r_o_m.py
@@ -29,21 +29,21 @@ async def get_markup(message: Message):
     inline_keyboard = []
     ikeyboard = []
     ikeyboard.append(InlineKeyboardButton(
-        "leech 🤔🤔",
+        "Upload To TG",
         callback_data=("leech").encode("UTF-8")
     ))
     ikeyboard.append(InlineKeyboardButton(
-        "youtube-dl",
+        "YoutubeDL",
         callback_data=("ytdl").encode("UTF-8")
     ))
     inline_keyboard.append(ikeyboard)
     ikeyboard = []
     ikeyboard.append(InlineKeyboardButton(
-        "A leech TAR . GZ  🤔🤔",
+        "Upload As TAR",
         callback_data=("leecha").encode("UTF-8")
     ))
     ikeyboard.append(InlineKeyboardButton(
-        "A youtube-dl TAR . GZ",
+        "A youtube-dl ",
         callback_data=("ytdla").encode("UTF-8")
     ))
     inline_keyboard.append(ikeyboard)
